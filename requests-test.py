8c1c88bf82641ac1e38271060556cb7d8a3f92e4
teltrivar <wolf.perry@gmail.com>
2017-01-23 10:03:05 -0700

    Signed-off-by: teltrivar <wolf.perry@gmail.com>

--- requests-test.py
+++ requests-test.py
@@ -1,6 +1,14 @@
-import requests
+import requests, pprint
+
+f = open('workfile.txt', 'w')
+pp = pprint.PrettyPrinter(indent=4,depth=6)
 
 with open('ids2test.txt','r') as handle:
     for line in handle:
-        req = requests.get(''.join(['https://sites-uat.ualberta.ca/~',line.rstrip()]))
-        print line.rstrip() + " | " + str(req.status_code) + " | " + req.headers['content-type']
+        req = requests.get(''.join(['https://sites-uat.ualberta.ca/~',line.rstrip(),'/']))
+        # pp.pprint(req.headers)
+        print line.rstrip() + " | " + str(req.status_code) + " | " + req.headers['content-type'] +"\n" 
+        value=(line.rstrip() + " | " + str(req.status_code) + " | " + req.headers['content-type'] +"\n" )
+        f.write(str(value))
+
+f.close()
